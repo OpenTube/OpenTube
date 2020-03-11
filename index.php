1d27a9aa0acb70694144c501c79783cb2e8d8c59
ChillerDragon <ChillerDragon@gmail.com>
2020-03-11 20:52:15 +0100

    Fix name

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FooTube</title>
+    <title>OpenTube</title>
 </head>
-<h1>FooTube</h1>
+<h1>OpenTube</h1>
 <?php
     if ($handle = opendir('videos')) {
         while (false !== ($entry = readdir($handle))) {
