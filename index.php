cb609ee29714a473e2d8e977a933cca9d65c945d
ChillerDragon <ChillerDragon@gmail.com>
2020-03-12 14:54:50 +0100

    Save and delete videos (closed #1)

--- index.php
+++ index.php
@@ -12,21 +12,65 @@
     <input type="submit">
     </form>
 <?php
-    if ($handle = opendir('videos')) {
-        while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != "..") {
-            $ext = pathinfo($entry, PATHINFO_EXTENSION);
-            echo "<h1>$entry</h1>";
-            echo '<video width="320" height="240" controls>';
-            echo "<source src=\"videos/$entry\" type=\"video/$ext\">";
-            echo '
-                Your browser does not support the video tag.
-                </video>
-            ';
+    function html_video_viewer($video, $saved) {
+        $ext = pathinfo($video, PATHINFO_EXTENSION);
+        echo "<h1>$video</h1>";
+        echo '<video width="320" height="240" controls>';
+        echo "<source src=\"" . (($saved) ? "saved_" : "") . "videos/$video\" type=\"video/$ext\">";
+        echo '
+            Your browser does not support the video tag.
+            </video>
+        ';
+    }
+    function html_video_buttons($video) {
+        echo "<br><a href='/index.php/?delete=$video'>DELETE</a><br>";
+        echo "<br><a href='/index.php/?save=$video'>SAVE</a><br>";
+    }
+    function save_video($video) {
+        if (!is_dir('saved_videos')) {
+            mkdir('saved_videos', 0777, true);
+        }
+        $full_path = 'videos/' . $video;
+        if (!is_file($full_path)) {
+            echo "<br>Error: file '$full_path' does not exist.<br>";
+            return;
+        }
+        rename($full_path, 'saved_videos/' . $video);
+    }
+    function delete_video($video) {
+        if (!is_dir('videos')) {
+            echo "<br>Error: videos directory not found";
+            return;
+        }
+        if (!is_file('videos/' . $video)) {
+            echo "<br>Error: file '$video' does not exist.<br>";
+            return;
+        }
+        unlink('videos/' . $video);
+    }
+    function html_video($video, $saved) {
+        html_video_viewer($video, $saved);
+        if (!$saved) {
+            html_video_buttons($video);
         }
     }
-    closedir($handle);
-}
+    function list_video_dir($dir, $saved) {
+        if ($handle = opendir($dir)) {
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry != "." && $entry != "..") {
+                    html_video($entry, $saved);
+                }
+            }
+            closedir($handle);
+        }
+    }
+    list_video_dir('videos', false);
+    list_video_dir('saved_videos', true);
+    if (isset($_GET['delete'])) {
+        delete_video($_GET['delete']);
+    } else if (isset($_GET['save'])) {
+        save_video($_GET['save']);
+    }
 ?>
 </body>
 </html>
