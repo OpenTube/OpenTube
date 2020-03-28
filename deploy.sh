84e82e9b157fb83b2e67bf3462e897ea896f900c
ChillerDragon <ChillerDragon@gmail.com>
2020-03-28 18:36:58 +0100

    Made deploy script lill more dynamic

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,19 @@ then
     exit 1
 fi
 
-scp -r ./* chiller@149.202.127.134:/var/www/html/OpenTube
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]
+then
+    echo "usage: $(basename "$0") [target] [host]"
+    echo "description: will scp all contents to host/target"
+    echo "example:    $(basename "$0") foo root@localhost:/var/www"
+    echo 'results in: scp -r ./* "root@localhost:/var/www/foo"'
+    exit 0
+fi
+
+target="${1:-OpenTube}"
+host="${2:-chiller@149.202.127.134:/var/www/html}"
+
+echo "[*] uploading to '$host/$target' ..."
+
+scp -r ./* "$host/$target"
 
