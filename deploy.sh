a78ada3d6eba438ed028f786d231118cdf44ce64
ChillerDragon <ChillerDragon@gmail.com>
2020-04-22 16:09:25 +0200

    Do not deploy local testing videos

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,18 @@ fi
 
 host="${1:-chiller@149.202.127.134:/var/www/html/OpenTube}"
 
+function upload_cmd() {
+    file="$1"
+    cmd="scp -r ./$file $host"
+    echo "[*] $cmd"
+    eval "$cmd"
+}
+
 echo "[*] uploading to '$host' ..."
-cmd="scp -r ./* $host"
-echo "[*] $cmd"
-eval "$cmd"
+
+upload_cmd *.html
+upload_cmd *.htm
+upload_cmd *.php
+upload_cmd *.sh
+# upload_cmd .git
 
