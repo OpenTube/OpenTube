d10fcedaa0f189b3b7833e82c41e10c0002f0ed2
ChillerDragon <chillerdragon@gmail.com>
2020-11-14 17:25:08 +0100

    Add custom css

--- download.php
+++ download.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/basic.css">
-    <title>OpenTube</title>
+<?php require 'php/head.php'; ?>
 </head>
 <body>
     <div class="content">
---
+++ php/head.php
@@ -0,0 +1,10 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/basic.css">
+    <title>OpenTube</title>
+
+<?php
+    if (file_exists('css/custom.css')) {
+        echo '<link rel="stylesheet" type="text/css" href="css/custom.css">';
+    }
+?>
