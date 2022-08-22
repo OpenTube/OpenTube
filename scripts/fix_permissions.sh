c11d8aa6c1b0e8d39e83c3fba8e1000ae6a51703
ChillerDragon <ChillerDragon@gmail.com>
2022-08-22 15:48:56 +0200

    Improve fix permissions script

--- scripts/fix_permissions.sh
+++ scripts/fix_permissions.sh
@@ -11,6 +11,42 @@
 # recursivley give write permission to the owning group on all video folders
 # should be run every time a user folder is added
 
+function check_dir() {
+	local dir="$1"
+	if [ ! -d "$dir" ]
+	then
+		echo "Error: directory $dir not found"
+		echo "       you have in the root of OpenTube"
+		exit 1
+	fi
+}
+check_dir scripts
+check_dir videos
+check_dir .git
+check_dir php
+
+webgroup="$(getent group | cut -d: -f1 | grep -E '^(www-data|http)$' | tail -n 1)"
+
+if [ "$USER" == "" ]
+then
+	echo 'Error: $USER variable not set'
+	exit 1
+fi
+
+echo "Detected user: $USER"
+if [ "$webgroup" == "" ]
+then
+	echo "Enter group name of web server:"
+	read -r webgroup
+else
+	echo "Detected web server group: $webgroup"
+fi
+
+sudo chown -R "$USER:$webgroup" videos
+sudo chown -R "$USER:$webgroup" db
+
+sudo chmod -R g+w db
+
 sudo chmod -R g+w videos
 sudo chmod -R u+w videos
 
