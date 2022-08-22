33be2cf7b121eb6606dd264cc975a39e6c0cceae
ChillerDragon <ChillerDragon@gmail.com>
2022-08-22 16:32:38 +0200

    Stricten slugger to not allow dots

--- scripts/slug_video_names.sh
+++ scripts/slug_video_names.sh
@@ -40,20 +40,34 @@ function save_mv() {
 }
 
 function slug_video() {
-    local filename="$1"
-    local path
-    local filename_slug
-    path="$(dirname "$filename")"
-    filename="$(basename "$filename")"
-    filename_slug="${filename//[^a-zA-Z0-9\._-]/_}"
-    if [ "$filename" != "$filename_slug" ]
-    then
-        printf '\033[1m"\033[0m%s\033[1m" -> "\033[0m%s\033[1m"\033[0m\n' \
-            "$(f_chomp "$filename")" "$(f_chomp "$filename_slug")"
-        filename_slug="$path/$filename_slug"
-        save_mv "$f" "$filename_slug"
-        c="$((c+1))"
-    fi
+	local filename="$1"
+	local path
+	local filename_slug
+	if [[ ! "$filename" =~ \. ]]
+	then
+		echo "Error: file does not contain at least one dot"
+		echo ""
+		echo "  $filename"
+		echo ""
+		exit 1
+	fi
+	local ext
+	path="$(dirname "$filename")"
+	filename="$(basename "$filename")"
+	ext="${filename##*.}"      # cut off extension to slug dots
+	filename_no_ext="${filename%.*}"  # cut off extension to slug dots
+	filename_slug="${filename_no_ext//[^a-zA-Z0-9_-]/_}"
+	filename_slug="$filename_slug.$ext"
+	if [ "$filename" != "$filename_slug" ]
+	then
+		echo "$filename"
+		echo "$filename_slug"
+		printf '\033[1m"\033[0m%s\033[1m" -> "\033[0m%s\033[1m"\033[0m\n' \
+			"$(f_chomp "$filename")" "$(f_chomp "$filename_slug")"
+		filename_slug="$path/$filename_slug"
+		save_mv "$f" "$filename_slug"
+		c="$((c+1))"
+	fi
 }
 
 for f in ./videos/{saved,downloaded,unlisted}/*.{flv,mp4,webm}
