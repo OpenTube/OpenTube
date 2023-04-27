9c7b19c836c06de09efa881e729f954017a0218c
ChillerDragon <ChillerDragon@gmail.com>
2023-04-27 17:52:05 +0200

    Fix test token endpoint

--- php/test_token.php
+++ php/test_token.php
@@ -5,10 +5,12 @@ require_once 'tokens.php';
 // TODO: add ratelimits here
 //       this should be a healthcheck endpoint not a bruteforce endpoint
 
-$username = $_POST['username'];
-$token = $_POST['token'];
+// prefer post for security
+// fallback to get for easier debugging
+$username = isset($_POST['username']) ? $_POST['username'] : $_GET['username'];
+$token = isset($_POST['token']) ? $_POST['token'] : $_GET['token'];
 
-if (is_valid_token($usernam, $token)) {
+if (is_valid_token($username, $token)) {
     echo "OK";
 } else {
     echo "invalid token";
