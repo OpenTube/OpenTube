d362ceb86918ff85daa10c61a2f4bf1dc8ad5be9
ChillerDragon <chillerdragon@gmail.com>
2024-11-09 16:17:44 +0800

    Generate tmp tokens

--- scripts/add_all_videos.sh
+++ scripts/add_all_videos.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
-# As a quick hack I created tokens for all users from the web view
-# Then I ran this sql query to set all tokens to "token"
-#
-#  UPDATE Tokens SET UUID = 'token';
-#
-# ran the script
-#
-#  DELETE FROM Tokens;
-#
-# But in the future there should be a admin token that can access
-# data from other users
-# and this token can then be used by the script
+OPENTUBE_TOKEN="$(cat /proc/sys/kernel/random/uuid)"
 
-OPENTUBE_TOKEN=token
+get_id_by_username() {
+	local username="$1"
+	sqlite3 -init /dev/null db/opentube.db < <(echo "SELECT ID FROM Users WHERE Username = '$username';")
+}
+
+insert_token_query() {
+	local username="$1"
+	local user_id
+	user_id="$(get_id_by_username "$username")"
+	if [ "$user_id" = "" ]
+	then
+		echo "Error: user with name '$username' not found." 1>&2
+		exit 1
+	fi
+	local expire_date
+	local issue_date
+	expire_date="$(date '+%F %H:%M:%S' -d '+3 days')"
+	issue_date="$(date '+%F %H:%M:%S')"
+	cat <<-EOF
+	INSERT INTO Tokens
+	(UUID, Title, Username, UserID, ExpireDate, IssueDate, IssuerIp) VALUES
+	('$OPENTUBE_TOKEN', 'add_all_script_xxx', '$username', $user_id, '$expire_date', '$issue_date', '127.0.0.1');
+	EOF
+}
+
+cleanup_tmp_tokens() {
+	sqlite3 db/opentube.db < <(echo "DELETE FROM Tokens WHERE Title = 'add_all_script_xxx';")
+}
+
+add_tmp_token_for_user() {
+	local username="$1"
+	sqlite3 db/opentube.db < <(insert_token_query "$username")
+}
 
 for user_path in videos/users/*/
 do
 	[[ -d "$user_path" ]] || continue
 
 	username="$(basename "$user_path")"
+
+	echo "[$username] generating tmp token $OPENTUBE_TOKEN ..."
+	add_tmp_token_for_user "$username"
+
 	for filepath in "$user_path"*.mp4
 	do
 		[[ -f "$filepath" ]] || continue
@@ -33,4 +58,7 @@ do
 			--data-raw "title=$title&description=todo&filepath=$filename&token=$OPENTUBE_TOKEN&username=$username"
 	done
 done
+
+echo "[done] cleaning up tmp tokens ..."
+cleanup_tmp_tokens
 
